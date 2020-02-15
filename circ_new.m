dc3d7c69735ccf3133db44b26a8b4c919b9b0118
Seong Woo Han <seongwoohan29@gmail.com>
2020-02-15 15:28:42 -0500

    Update circ_new.m

--- circ_new.m
+++ circ_new.m
@@ -3,9 +3,9 @@
 clear all % clear all variables
 close all       % and figures
 
-asd_vec = [0 ] % 0 10 100 1000 10000
-vsd_vec = [0 ] % 0 50 100 1000 10000
-d_vec = [0 3 4 6 10 100] % 0 10 100 1000 10000
+asd_vec = [0 ] % 0 10 100 500 800 1000 2000 3000 5000 10000
+vsd_vec = [0 ] % 0 0.5 3.5 5 7 10 13 16 20 30 50 70 80 100 500 800 1000 1500 3000
+d_vec = [0] % 0 0.1 0.3 0.5 0.7 1 3 4 6 10 30 50 70 100 500 1000
 
 ncase = 5;
 q_asd_vecA = zeros(1,ncase);
@@ -53,64 +53,40 @@ for iDD = 1:length(d_vec)
     psa_d_vecD(iDD) = meanvalue(P_plot(isa,:), klokmax, T, dt, 10);
 end 
 
-figure(200)
-subplot(3,1,1),plot(q_asd_vecA, '-o')
-title('Flow between Right and Left Atrium')
-xlabel('Shunt Flow') 
-ylabel('Shunt Mean')
-subplot(3,1,2),plot(ppa_asd_vecA, '-o')
-title('Flow between Right and Left Atrium')
-xlabel('Shunt Flow') 
-ylabel('Pressure')
-subplot(3,1,3),plot(psa_asd_vecA, '-o')
-title('Flow between Right and Left Atrium')
-xlabel('Shunt Flow') 
-ylabel('Pressure')
-
-
 figure(201)
 plot(q_asd_vecA, ppa_asd_vecA, '-ro', q_asd_vecA, psa_asd_vecA, '-bs')
 title('Atirum Shunt Flow between Pulmonary Artery Pressure and Systemic Artery Pressure')
 legend({'Pulmonary artery P','Systemic artery P'},'Location', 'east') 
-xlabel('Shunt Flow') 
+xlabel('Shunt Flow Mean') 
 ylabel('Pressure')
 
 figure(202)
 plot(q_asd_vecA, ppa_asd_vecA, '-ro')
 title('Atirum Shunt Flow between Pulmonary Artery Pressure')
-xlabel('Shunt Flow') 
+xlabel('Shunt Flow Mean') 
 ylabel('Pressure')
 
 figure(203)
 plot(q_asd_vecA, psa_asd_vecA, '-bs')
 title('Atirum Shunt Flow between Systemic Artery Pressure')
-xlabel('Shunt Flow') 
+xlabel('Shunt Flow Mean') 
 ylabel('Pressure')
 
-figure(300)
-subplot(3,1,1),plot(q_vsd_vecV, '-o')
-subplot(3,1,2),plot(ppa_vsd_vecV, '-o')
-subplot(3,1,3),plot(psa_vsd_vecV, '-o')
 
 figure(301)
 plot(q_vsd_vecV, ppa_vsd_vecV, '-ro', q_vsd_vecV, psa_vsd_vecV, '-bs')
 title('Ventricle Shunt Flow between Pulmonary Artery Pressure and Systemic Artery Pressure')
 legend({'Pulmonary artery P','Systemic artery P'},'Location', 'east') 
-xlabel('Shunt Flow') 
+xlabel('Shunt Flow Mean') 
 ylabel('Pressure')
 
-figure(400)
-subplot(3,1,1),plot(q_d_vecD, '-o')
-subplot(3,1,2),plot(ppa_d_vecD, '-o')
-subplot(3,1,3),plot(psa_d_vecD, '-o')
 
 figure(401)
 plot(q_d_vecD, ppa_d_vecD, '-ro', q_d_vecD, psa_d_vecD, '-bs')
 title('Artery Shunt Flow between Pulmonary Artery Pressure and Systemic Artery Pressure')
 legend({'Pulmonary artery P','Systemic artery P'},'Location', 'east') 
-xlabel('Shunt Flow') 
+xlabel('Shunt Flow Mean') 
 ylabel('Pressure')
 
 %negative value will be ignored
 %semilogx(q_asd_vecA, psa_asd_vecA)
-
