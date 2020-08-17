9cb626f40715dda7690a51bc27553f167b59dd9d
Seong Woo Han <seongwoohan29@gmail.com>
2020-08-17 11:03:00 -0400

    Update circ_asd.m

--- src/circ_asd.m
+++ src/circ_asd.m
@@ -5,7 +5,11 @@ clear all % clear all variables
 close all       
 
 % asd shunt conductance (L/min/mmHg)
-asd_vec = [0 0.1 3 10 50 100 300 500 800 1000]% 5000 10000]
+jasd=7;
+jvsd=8;
+jd=9;
+j_shunt = jasd
+asd_vec = (0 : 0.1 : 1) / 100. % dm2
 ncase_asd = length(asd_vec);
 
 % each flow of interest
@@ -31,9 +35,7 @@ oxy_pa_asd_vecA = zeros(1,ncase_asd);
 
 % mean value for above variables
 for iA = 1:length(asd_vec)
-    asd = asd_vec(iA);
-    vsd=0; 
-    d=0;
+    Ashunt = asd_vec(iA);
     circ;     
     qAo_asd_vecA(iA) = meanvalue(Q_plot(jAo,:), klokmax, T, dt, 10);
     qs_asd_vecA(iA) = meanvalue(Q_plot(js,:), klokmax, T, dt, 10);
@@ -56,72 +58,67 @@ if (length(asd_vec) > 0)
 
 % ASD : shunt conductance & pressure
 figure(200)
-subplot(3,1,1),plot(asd_vec, ppa_asd_vecA, '-o', asd_vec, psa_asd_vecA, '-o','linewidth', 1.5)
-title('Atrial Septal Defect','FontSize',18,'FontWeight','bold')
-legend({'Pulmonary Artery', 'Systemic Artery'},'Location', 'east') 
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold')
-ylabel('Pressure (mmHg)','FontSize',15,'FontWeight','bold')
+subplot(3,1,1),plot(asd_vec*100, ppa_asd_vecA, '-o', asd_vec*100, psa_asd_vecA, '-o','linewidth', 2)
+title('Atrial Septal Defect')
+legend({'Pulmonary Artery', 'Systemic Artery'},'Location', 'east','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Pressure (mmHg)')
+set(gca,'FontSize',18)
 grid on
-subplot(3,1,2),plot(asd_vec, ppa_asd_vecA, '-bo','linewidth', 1.5)
-legend({'Pulmonary Artery'},'Location', 'northeast') 
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold')
-ylabel('Pressure (mmHg)','FontSize',15,'FontWeight','bold')
+subplot(3,1,2),plot(asd_vec*100, ppa_asd_vecA, '-bo','linewidth', 2)
+legend({'Pulmonary Artery'},'Location', 'east','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Pressure (mmHg)')
+set(gca,'FontSize',18)
 grid on
-subplot(3,1,3),plot(asd_vec, psa_asd_vecA, '-ro','linewidth', 1.5)
-legend({'Systemic Artery'},'Location', 'northeast') 
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold')
-ylabel('Pressure (mmHg)','FontSize',15,'FontWeight','bold')
+subplot(3,1,3),plot(asd_vec*100, psa_asd_vecA, '-ro','linewidth', 2)
+legend({'Systemic Artery'},'Location', 'east','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Pressure (mmHg)')
+set(gca,'FontSize',18)
 grid on
 
 
 % ASD : shunt conductance & shunt flow mean
 figure(201)
-plot(asd_vec, q_asd_mean_vecA, '-o', asd_vec, q_asd_plus_vecA, '-o', asd_vec, q_asd_minus_vecA, '-o','linewidth', 1.5)
-title('Atrial Septal Defect','FontSize',18,'FontWeight','bold')
-legend({'Mean Flow', 'Positive Flow', 'Negative Flow'},'Location', 'northwest') 
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold')
-ylabel('Shunt Flow Mean (L/min)','FontSize',15,'FontWeight','bold')
-grid on
-
-% ASD : shunt conductance & oxygen saturation 
-figure(203)
-subplot(3,1,1),semilogx(asd_vec, qs_asd_vecA, '-ro','linewidth', 1.5)
-title('Atrial Septal Defect: Systemic Artery','FontSize',18,'FontWeight','bold')
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15','FontWeight','bold') 
-ylabel('Systemic Flow (L/min)','FontSize',15','FontWeight','bold')
-grid on
-subplot(3,1,2),semilogx(asd_vec, 10.*oxy_sa_asd_vecA, '-bo','linewidth', 1.5)
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold') 
-ylabel('Oxygen Saturation (%)','FontSize',15,'FontWeight','bold')
-grid on
-subplot(3,1,3),semilogx(asd_vec, qs_asd_vecA.*oxy_sa_asd_vecA, '-mo','linewidth', 1.5)
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold') 
-ylabel('Oxygen Delivery (mmol/min)','FontSize',15,'FontWeight','bold')
+plot(asd_vec*100, q_asd_mean_vecA, '-o', asd_vec*100, q_asd_plus_vecA, '-o', asd_vec*100, q_asd_minus_vecA, '-o','linewidth', 2)
+title('Atrial Septal Defect')
+legend({'Mean Flow', 'Positive Flow', 'Negative Flow'},'Location', 'northwest','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Shunt Flow Mean (L/min)')
+set(gca,'FontSize',25)
 grid on
 
 % Without semilogx
-figure(204)
-subplot(3,1,1),plot(asd_vec, qs_asd_vecA, '-ro','linewidth', 1.5)
-title('Atrial Septal Defect: Systemic Artery','FontSize',18,'FontWeight','bold')
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15','FontWeight','bold') 
-ylabel('Systemic Flow (L/min)','FontSize',15','FontWeight','bold')
+figure(202)
+subplot(3,1,1),plot(asd_vec*100, qs_asd_vecA, '-ro','linewidth', 2)
+title('Atrial Septal Defect: Systemic Artery')
+xlabel('Shunt Area (cm^2)') 
+ylabel('Systemic Flow (L/min)')
+set(gca,'FontSize',18)
 grid on
-subplot(3,1,2),plot(asd_vec, 10.*oxy_sa_asd_vecA, '-bo','linewidth', 1.5)
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold') 
-ylabel('Oxygen Saturation (%)','FontSize',15,'FontWeight','bold')
+subplot(3,1,2),plot(asd_vec*100, 10.*oxy_sa_asd_vecA, '-bo','linewidth', 2)
+xlabel('Shunt Area (cm^2)') 
+ylabel('Oxygen Saturation (%)')
+set(gca,'FontSize',18)
 grid on
-subplot(3,1,3),plot(asd_vec, qs_asd_vecA.*oxy_sa_asd_vecA, '-mo','linewidth', 1.5)
-xlabel('Shunt Conductance (L/min/mmHg)','FontSize',15,'FontWeight','bold') 
-ylabel('Oxygen Delivery (mmol/min)','FontSize',15,'FontWeight','bold')
+subplot(3,1,3),plot(asd_vec*100, qs_asd_vecA.*oxy_sa_asd_vecA, '-mo','linewidth', 2)
+xlabel('Shunt Area (cm^2)') 
+ylabel('Oxygen Delivery (mmol/min)')
+set(gca,'FontSize',18)
 grid on
 
 % ASD : Flow on time dependence
 % axis([xmin xmax ymin ymax])
-figure(205)
-plot(t_plot,Q_plot(jasd,:),'linewidth', 1.5);
-title('Atrial Septal Defect Shunt Flow','FontSize',18,'FontWeight','bold')
-xlabel('Time (min)','FontSize',15,'FontWeight','bold') 
-ylabel('Blood Flow (L)','FontSize',15,'FontWeight','bold')
+figure(203)
+plot(t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jasd,((klokmax-((T/dt)*10)+1):klokmax)),'linewidth', 2);
+title('Atrial Septal Defect Shunt Flow')
+xlabel('Time (min)') 
+ylabel('Blood Flow (L/min)')
+yline(0, '--b','linewidth', 2)
+set(gca,'FontSize',20)
+ylim([-1.5 1.5])
+% 0 on the line
 
 %figure(206)
 %need this when calibrating data
