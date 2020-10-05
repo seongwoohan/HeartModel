bda48482923605a808c480981d260baa7b083ac4
Seong Woo Han <seongwoohan29@gmail.com>
2020-10-05 16:53:59 -0400

    Update circ_d.m

--- src/matlab_ver/circ_d.m
+++ src/matlab_ver/circ_d.m
@@ -1,6 +1,6 @@
 % The new main program and figures for ductance
 % variables for ductance
-
+tic
 clear all % clear all variables
 close all  
 
@@ -9,7 +9,7 @@ jasd=7;
 jvsd=8;
 jd=9;
 j_shunt = jd
-d_vec = [0.2]./100
+d_vec = (0 : 0.1 : 1) / 100
 % (0 : 0.1 : 1) / 100
 % 0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01
 
@@ -37,7 +37,7 @@ psa_d_vecD = zeros(1,ncase_d);
 oxy_sa_d_vecD = zeros(1,ncase_d);
 oxy_pa_d_vecD = zeros(1,ncase_d);
 
-num_cycles_for_mean = 3;
+num_cycles_for_mean = 10;
 
 % mean value for above variables
 for iDD = 1:length(d_vec)
@@ -65,101 +65,115 @@ if (length(d_vec) > 0)
 figure(1000)
 plot(t_plot, 10.*O2_plot,'linewidth',3)
 id = legend('LV','SA','SV','RV','PA','PV')
+xlabel('Time (min)') 
+ylabel('O2')
 set(gca,'FontSize',18)
 
 figure(1001)
 plot(t_plot, 10.*O2_plot(iLV,:),'linewidth',3)
+xlabel('Time (min)') 
+ylabel('O2 in left ventricle')
 id = legend('LV')
 set(gca,'FontSize',18)
 
 figure(1002)
 plot(t_plot, total_oxy_amt_plot,'linewidth',3)
+xlabel('Time (min)') 
+ylabel('Total oxygen amount')
 set(gca,'FontSize',18)
 
 figure(1003)
 plot(t_plot, O2_amt_plot,'linewidth',3)
 id = legend('LV','SA','SV','RV','PA','PV')
+xlabel('Time (min)') 
+ylabel('O2 amount')
 set(gca,'FontSize',18)
 
 figure(1004)
 plot(t_plot, V_plot,'linewidth',3)
 id = legend('LV','SA','SV','RV','PA','PV')
+xlabel('Time (min)') 
+ylabel('Volume')
 set(gca,'FontSize',18)
 
 % Ductus : shunt conductance & pressure
-%figure(400)
-%plot(d_vec*100, ppa_d_vecD, '-o', d_vec*100, psa_d_vecD, '-o','linewidth', 2)
-%title('Patent Ductus Arteriosus')
-%legend({'Pulmonary Artery', 'Systemic Artery'},'Location', 'east','FontSize',25) 
-%xlabel('Shunt Area (cm^2)')
-%ylabel('Pressure (mmHg)')
-%set(gca,'FontSize',25)
-%grid on
+figure(400)
+plot(d_vec*100, ppa_d_vecD, '-o', d_vec*100, psa_d_vecD, '-o','linewidth', 2)
+title('Patent Ductus Arteriosus')
+legend({'Pulmonary Artery', 'Systemic Artery'},'Location', 'east','FontSize',25) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Pressure (mmHg)')
+set(gca,'FontSize',25)
+grid on
 
 % Ductus : shunt conductance & shunt flow mean
-%figure(401)
-%plot(d_vec*100, q_d_mean_vecD,'-o',d_vec*100, q_d_plus_vecD,'-o', d_vec*100, q_d_minus_vecD,'-o','linewidth', 2)
-%title('Patent Ductus Arteriosus')
-%legend({'Overall Flow', 'Positive Flow', 'Negative Flow'},'Location', 'east','FontSize',25) 
-%xlabel('Shunt Area (cm^2)')
-%ylabel('Shunt Flow Mean (L/min)')
-%set(gca,'FontSize',25)
-%grid on
+figure(401)
+plot(d_vec*100, q_d_mean_vecD,'-o',d_vec*100, q_d_plus_vecD,'-o', d_vec*100, q_d_minus_vecD,'-o','linewidth', 2)
+title('Patent Ductus Arteriosus')
+legend({'Overall Flow', 'Positive Flow', 'Negative Flow'},'Location', 'east','FontSize',25) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Shunt Flow Mean (L/min)')
+set(gca,'FontSize',25)
+grid on
 
-%figure(402)
-%subplot(3,1,1),plot(d_vec*100, q_d_mean_vecD,'-bo','linewidth', 2)
-%title('Patent Ductus Arteriosus')
-%legend({'Overall Flow'},'Location', 'east','FontSize',20) 
-%xlabel('Shunt Area (cm^2)')
-%ylabel('Shunt Flow Mean (L/min)')
-%set(gca,'FontSize',18)
-%grid on
-%subplot(3,1,2),plot(d_vec*100, q_d_plus_vecD,'r-o','linewidth', 2)
-%legend({'Positive Flow'},'Location', 'east','FontSize',20) 
-%xlabel('Shunt Area (cm^2)')
-%ylabel('Shunt Flow Mean (L/min)')
-%set(gca,'FontSize',18)
-%grid on
-%subplot(3,1,3),plot(d_vec*100, q_d_minus_vecD,'-yo','linewidth', 2)
-%legend({'Negative Flow'},'Location', 'northeast','FontSize',20) 
-%xlabel('Shunt Area (cm^2)')
-%ylabel('Shunt Flow Mean (L/min)')
-%set(gca,'FontSize',18)
-%grid on
+figure(402)
+subplot(3,1,1),plot(d_vec*100, q_d_mean_vecD,'-bo','linewidth', 2)
+title('Patent Ductus Arteriosus')
+legend({'Overall Flow'},'Location', 'east','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Shunt Flow Mean (L/min)')
+set(gca,'FontSize',18)
+grid on
+subplot(3,1,2),plot(d_vec*100, q_d_plus_vecD,'r-o','linewidth', 2)
+legend({'Positive Flow'},'Location', 'east','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Shunt Flow Mean (L/min)')
+set(gca,'FontSize',18)
+grid on
+subplot(3,1,3),plot(d_vec*100, q_d_minus_vecD,'-yo','linewidth', 2)
+legend({'Negative Flow'},'Location', 'northeast','FontSize',20) 
+xlabel('Shunt Area (cm^2)')
+ylabel('Shunt Flow Mean (L/min)')
+set(gca,'FontSize',18)
+grid on
 
 % Ductus : shunt conductance & oxygen saturation 
 % Without semilogx 
-%figure(403)
-%subplot(3,1,1),plot(d_vec*100, qs_d_vecD, '-ro','linewidth', 2)
-%title('Patent Ductus Arteriosus: Systemic Artery')
-%xlabel('Shunt Area (cm^2)') 
-%ylabel('Systemic Flow (L/min)')
-%set(gca,'FontSize',18)
-%grid on
-%subplot(3,1,2),plot(d_vec*100, 10.*oxy_sa_d_vecD, '-bo','linewidth', 2)
-%xlabel('Shunt Area (cm^2)') 
-%ylabel('Oxygen Saturation (%)')
-%set(gca,'FontSize',18)
-%grid on
-%subplot(3,1,3),plot(d_vec*100, qs_d_vecD.*oxy_sa_d_vecD, '-mo','linewidth', 2)
-%xlabel('Shunt Area (cm^2)') 
-%ylabel('Oxygen Delivery (mmol/min)')
-%set(gca,'FontSize',18)
-%grid on
+figure(410)
+plot(d_vec*100, qs_d_vecD, '-ro','linewidth', 2)
+title('Patent Ductus Arteriosus: Systemic Artery')
+xlabel('Shunt Area (cm^2)') 
+ylabel('Systemic Flow (L/min)')
+set(gca,'FontSize',18)
+grid on
+figure(411)
+plot(d_vec*100, 10.*oxy_sa_d_vecD, '-bo','linewidth', 2)
+title('Patent Ductus Arteriosus: Oxygen Saturation')
+xlabel('Shunt Area (cm^2)') 
+ylabel('Oxygen Saturation (%)')
+set(gca,'FontSize',18)
+grid on
+figure(412)
+plot(d_vec*100, qs_d_vecD.*oxy_sa_d_vecD, '-mo','linewidth', 2)
+title('Patent Ductus Arteriosus: Oxygen Delivery')
+xlabel('Shunt Area (cm^2)') 
+ylabel('Oxygen Delivery (mmol/min)')
+set(gca,'FontSize',18)
+grid on
 
 % Ductus : Flow on time dependence
-%figure(404)
-%plot(t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jd,((klokmax-((T/dt)*10)+1):klokmax)),'linewidth', 1.5);
-%title('Patent Ductus Arteriosus Shunt Flow')
-%xlabel('Time (min)') 
-%ylabel('Blood Flow (L/min)')
-%set(gca,'FontSize',18)
-%ylim([0 8])
-%yline(0, '--b','linewidth', 2)
-% draw a line on the 0 
+figure(404)
+plot(t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jd,((klokmax-((T/dt)*10)+1):klokmax)),'linewidth', 1.5);
+title('Patent Ductus Arteriosus Shunt Flow')
+xlabel('Time (min)') 
+ylabel('Blood Flow (L/min)')
+set(gca,'FontSize',18)
+ylim([0 8])
+yline(0, '--b','linewidth', 2)
+%draw a line on the 0 
 
-%figure(406)
-%plot(t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jAo,((klokmax-((T/dt)*10)+1):klokmax)),'linewidth', 1.5);
+figure(406)
+plot(t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jAo,((klokmax-((T/dt)*10)+1):klokmax)),t_plot((klokmax-((T/dt)*10)+1):klokmax), Q_plot(jd,((klokmax-((T/dt)*10)+1):klokmax)),'linewidth', 1.5)
 %title('Aortic Valve Flow','FontSize',18,'FontWeight','bold')
 %xlabel('Time (min)','FontSize',15,'FontWeight','bold') 
 %ylabel('Blood Flow (L)','FontSize',15,'FontWeight','bold')
@@ -189,3 +203,4 @@ set(gca,'FontSize',18)
 
 
 end
+toc
